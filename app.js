99ffffd82d6855a1b601246968b78f8e07a6bb62
BrianLevin <bml201095@gmail.com>
2021-04-19 09:48:33 -0400

    required express and body parser files, created variable which will hold express, and created local host sevrer

---
+++ app.js
@@ -0,0 +1,12 @@
+const express  = required("express");
+const bodyParser = required ("body-parser");
+
+const app = express()
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000!");
+  });
