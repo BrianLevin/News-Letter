1ff79f478628c98ad7fbcfcb9f6ea206de314863
BrianLevin <bml201095@gmail.com>
2021-04-20 11:56:00 -0400

    set up a successful api key which will post data to the mail chimp server

--- app.js
+++ app.js
@@ -1,74 +1,116 @@
 
+
 const express = require("express");
-const bodyParser = require ("body-parser");
+
+const bodyParser = require("body-parser");
+
 const request = require("request");
 
-const https= require ("https");
+const https = require("https");
+
+
 
 const app = express();
 
+
+
 app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function (req, res) {
 
-    res.sendFile (__dirname  + "/signup.html");
+
+app.get("/", function(req, res){
+
+res.sendFile(__dirname + "/signup.html")
 
 });
 
-app.post ("/", function (req,res){
 
-    const firstName = req.body.fName;
-    const lastName = req.body.lName;
-    const email = req.body.email;
 
- const data=  {
-members:  [
+app.post("/", function(req, res){
+
+const firstName = req.body.fName;
+
+const lastName = req.body.lName;
+
+const email = req.body.email;
+
+
+
+const data = {
+
+members:[
+
 {
-    email_address: email,
-    status: "subscribed",
-    merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName
-    }
 
+email_address: email,
 
+status: "subscribed",
+
+merge_fields: {
+
+FNAME: firstName,
+
+LNAME: lastName
+
+}
 
 }
 
 ]
 
- }
+}
+
+
+
 const jsonData = JSON.stringify(data);
 
-const url =  "https://us1.api.mailchimp.com/3.0/lists/69c14344df";
+
+
+const url ="https://us1.api.mailchimp.com/3.0/lists/69c14344df";
+
+
 
 const options = {
 
 method: "POST",
-auth:  "Brian:c92f3f4c13f9e37945b1d2112a4d6b15-us1"
+
+auth: "Brian:2accfc82cdafa2bc780f2d928bbe8b97-us1"
 
 }
 
- const request= https.request(url,  options, function (responce){
 
 
-responce.on("data", function(data){
+const request = https.request(url, options, function(response){
+
+response.on("data", function(data){
+
 console.log(JSON.parse(data));
-
 
 });
 
-})
+});
+
+
+
 request.write(jsonData);
 
 request.end();
+
+
+
 });
 
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000!");
-  });
+
+app.listen(3000, function(){
+
+console.log("Server is running on port 3000!");
+
+});
+
+
 
 
   // API KEY
