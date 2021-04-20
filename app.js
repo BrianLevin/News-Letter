f6dac637f21f05934f81ee3e9530414de2ef73da
BrianLevin <bml201095@gmail.com>
2021-04-20 12:23:55 -0400

    created a route which will send user to success or failure htmp pages

--- app.js
+++ app.js
@@ -86,10 +86,10 @@ const request = https.request(url, options, function(response){
 
     if (response.statusCode === 200) {
 
-        res.send("Successfully subscribed!");
+        res.sendFile( __dirname + "/success.html");
 
     } else {
-res.send("There was an error with signing up, please try again!");
+res.sendFile( __dirname + "/failure.html");
 
     }
 
